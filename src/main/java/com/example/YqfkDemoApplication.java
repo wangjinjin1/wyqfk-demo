42a615497aa85b38661687c187a422a5fb5ced99
wjj-maker <2836007460@qq.com>
2021-04-19 09:52:25 +0800

    add dependency

---
+++ src/main/java/com/example/YqfkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YqfkDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YqfkDemoApplication.class, args);
+    }
+
+}
